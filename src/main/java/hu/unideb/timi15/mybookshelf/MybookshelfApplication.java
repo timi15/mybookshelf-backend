aa850340bbde7e46d0a698ea7f95443a175a3aee
timi15 <vargatimi505@gmail.com>
2025-10-14 15:23:50 +0200

    Initial commit

---
+++ src/main/java/hu/unideb/timi15/mybookshelf/MybookshelfApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.timi15.mybookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybookshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/timi15/mybookshelf/MybookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.timi15.mybookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
