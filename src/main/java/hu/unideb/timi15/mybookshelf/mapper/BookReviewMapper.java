e9692ce40dcfcc896e4ef6040b2cfc6d21beb6d7
timi15 <vargatimi505@gmail.com>
2025-10-15 19:58:58 +0200

    refactor(mapping): add BookReviewMapper and DateConverter

---
+++ src/main/java/hu/unideb/timi15/mybookshelf/mapper/BookReviewMapper.java
@@ -0,0 +1,24 @@
+package hu.unideb.timi15.mybookshelf.mapper;
+
+import hu.unideb.timi15.mybookshelf.entity.BookReviewEntity;
+import hu.unideb.timi15.mybookshelf.service.dto.bookreview.request.CreateBookReviewRequestDTO;
+import hu.unideb.timi15.mybookshelf.service.dto.bookreview.response.BookReviewResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", uses = {DateConverter.class}, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface BookReviewMapper {
+
+    @Mapping(target = "startDate", source = "bookReviewEntity.startDate", qualifiedByName = "timestampToLocalDate")
+    @Mapping(target = "finishDate", source = "bookReviewEntity.finishDate", qualifiedByName = "timestampToLocalDate")
+    BookReviewResponseDTO toResponseDTO(BookReviewEntity bookReviewEntity);
+
+
+    @Mapping(target = "documentId", ignore = true)
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "startDate", source = "createBookReviewRequestDTO.startDate", qualifiedByName = "localDateToTimestamp")
+    @Mapping(target = "finishDate", source = "createBookReviewRequestDTO.finishDate", qualifiedByName = "localDateToTimestamp")
+    BookReviewEntity toEntity(CreateBookReviewRequestDTO createBookReviewRequestDTO);
+
+}
