06989e525b627f445b7fb57123931deded29fb37
timi15 <vargatimi505@gmail.com>
2026-01-08 14:42:29 +0100

    refactor: Initialize Firebase credentials from FIREBASE_CREDENTIALS_JSON

--- src/main/java/hu/unideb/timi15/mybookshelf/config/FirebaseConfig.java
+++ src/main/java/hu/unideb/timi15/mybookshelf/config/FirebaseConfig.java
@@ -9,20 +9,29 @@ import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @Slf4j
 public class FirebaseConfig {
 
-    @Value("${firebase.config.file}")
-    private Resource serviceAccount;
+//    @Value("${firebase.config.file}")
+//    private Resource serviceAccount;
 
     @PostConstruct
     public void initializeFirebase() throws IOException {
 
+        String firebaseCredentialsJson = System.getenv("FIREBASE_CREDENTIALS_JSON");
+
+        InputStream credentialsStream = new ByteArrayInputStream(
+                firebaseCredentialsJson.getBytes(StandardCharsets.UTF_8)
+        );
+
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                .setCredentials(GoogleCredentials.fromStream(credentialsStream))
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
