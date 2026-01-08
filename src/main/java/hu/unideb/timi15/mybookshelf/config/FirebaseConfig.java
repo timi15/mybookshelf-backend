a8e96c2d192b227d9ca96d6f9fa42f620acfa39e
timi15 <vargatimi505@gmail.com>
2026-01-08 15:49:06 +0100

    refactor: refactor application.properties

--- src/main/java/hu/unideb/timi15/mybookshelf/config/FirebaseConfig.java
+++ src/main/java/hu/unideb/timi15/mybookshelf/config/FirebaseConfig.java
@@ -1,11 +1,17 @@
 package hu.unideb.timi15.mybookshelf.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.cloud.StorageClient;
+import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -44,6 +50,30 @@ public class FirebaseConfig {
         log.info("Firebase config initialized from environment variables");
 
         return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    @DependsOn(value = "createFireBaseApp")
+    public Firestore createFirestoreClient() {
+        return FirestoreClient.getFirestore();
+    }
+
+    @Bean
+    @DependsOn(value = "createFireBaseApp")
+    public StorageClient createFirebaseStorage() {
+        return StorageClient.getInstance();
+    }
+
+    @Bean
+    @DependsOn(value = "createFireBaseApp")
+    public FirebaseAuth createFirebaseAuth() {
+        return FirebaseAuth.getInstance();
+    }
+
+    @Bean
+    @DependsOn(value = "createFireBaseApp")
+    public FirebaseMessaging createFirebaseMessaging() {
+        return FirebaseMessaging.getInstance();
     }
 
 }
