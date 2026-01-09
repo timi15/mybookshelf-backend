a456f57a88f9b66485d991bf85a5cf4f238c059c
timi15 <vargatimi505@gmail.com>
2026-01-09 18:41:06 +0100

    fix(cors): add GitHub Pages domain to allowed origins

--- src/main/java/hu/unideb/timi15/mybookshelf/config/CorsConfig.java
+++ src/main/java/hu/unideb/timi15/mybookshelf/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://timi15.github.io")
+                        .allowedOrigins("http://localhost:3000", "https://timi15.github.io/mybookshelf/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
